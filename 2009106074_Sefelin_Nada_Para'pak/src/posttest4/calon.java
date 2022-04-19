49a5aff4e25bcc48fd6e2a3e88c6dcbd2be5ecd6
SefelinNadaParapak <sefelinnada@gmail.com>
2022-04-19 19:53:18 +0800

    posttest4

---
+++ 2009106074_Sefelin_Nada_Para'pak/src/posttest4/calon.java
@@ -0,0 +1,37 @@
+package posttest4;
+
+public class calon {
+    protected String namacalonkepaladaerah, namacalonwakil, nomorurut; 
+
+    public calon(String namacalonkepaladaerah, String namacalonwakil, String nomorurut) {
+        this.namacalonkepaladaerah = namacalonkepaladaerah;
+        this.namacalonwakil = namacalonwakil;
+        this.nomorurut = nomorurut;
+    }
+
+    public String getNamacalonkepaladaerah() {
+        return namacalonkepaladaerah;
+    }
+
+    public void setNamacalonkepaladaerah(String namacalonkepaladaerah) {
+        this.namacalonkepaladaerah = namacalonkepaladaerah;
+    }
+
+    public String getNamacalonwakil() {
+        return namacalonwakil;
+    }
+
+    public void setNamacalonwakil(String namacalonwakil) {
+        this.namacalonwakil = namacalonwakil;
+    }
+
+    public String getNomorurut() {
+        return nomorurut;
+    }
+
+    public void setNomorurut(String nomorurut) {
+        this.nomorurut = nomorurut;
+    }
+
+
+}
